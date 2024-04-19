65a782518821d5ae2d9bbb446f4b18bdd40b99d3
FabianReyna <3753836700101@ingenieria.usac.edu.gt>
2024-04-19 16:33:16 -0600

    Correccion clase 12
    Se corrige la llamada a funciones para que sea 100% funcional

--- Funciones/src/controllers/analisis/instrucciones/Declaracion.ts
+++ Funciones/src/controllers/analisis/instrucciones/Declaracion.ts
@@ -7,24 +7,45 @@ import Tipo, { tipoDato } from '../simbolo/Tipo'
 
 export default class Declaracion extends Instruccion {
     private identificador: string
-    private valor: Instruccion
+    private valor: Instruccion | undefined
 
-    constructor(tipo: Tipo, linea: number, col: number, id: string, valor: Instruccion) {
+    constructor(tipo: Tipo, linea: number, col: number, id: string, valor?: Instruccion) {
         super(tipo, linea, col)
         this.identificador = id
         this.valor = valor
     }
 
     interpretar(arbol: Arbol, tabla: tablaSimbolo) {
-        let valorFinal = this.valor.interpretar(arbol, tabla)
-        if (valorFinal instanceof Errores) return valorFinal
+        if (this.valor) {
+            let valorFinal = this.valor?.interpretar(arbol, tabla)
+            if (valorFinal instanceof Errores) return valorFinal
 
-        if (this.valor.tipoDato.getTipo() != this.tipoDato.getTipo()) {
-            return new Errores("SEMANTICO", "No se puede declarar variable", this.linea, this.col)
+            if (this.valor.tipoDato.getTipo() != this.tipoDato.getTipo()) {
+                return new Errores("SEMANTICO", "No se puede declarar variable", this.linea, this.col)
+            }
+            console.log("validacion1")
+            if (!tabla.setVariable(new Simbolo(this.tipoDato, this.identificador, valorFinal))) {
+                return new Errores("SEMANTICO", "No se puede declarar variable porque ya existia", this.linea, this.col)
+            }
+        } else {
+            if (!tabla.setVariable(new Simbolo(this.tipoDato, this.identificador, this.valoresDefault(this.tipoDato.getTipo())))) {
+                return new Errores("SEMANTICO", "No se puede declarar variable porque ya existia", this.linea, this.col)
+            }
         }
-        console.log("validacion1")
-        if (!tabla.setVariable(new Simbolo(this.tipoDato, this.identificador, valorFinal))){
-            return new Errores("SEMANTICO", "No se puede declarar variable porque ya existia", this.linea, this.col)
+
+    }
+
+    //valores por default
+    valoresDefault(tipo: tipoDato) {
+        switch (tipo) {
+            case tipoDato.BOOL:
+                return true;
+            case tipoDato.CARACTER:
+                return ''
+            case tipoDato.ENTERO:
+                return 0
+            case tipoDato.DECIMAL:
+                return 0.0
         }
     }
 
--- Funciones/src/controllers/analisis/instrucciones/Llamada.ts
+++ Funciones/src/controllers/analisis/instrucciones/Llamada.ts
@@ -3,6 +3,7 @@ import Errores from "../excepciones/Errores";
 import Arbol from "../simbolo/Arbol";
 import tablaSimbolo from "../simbolo/tablaSimbolos";
 import Tipo, { tipoDato } from "../simbolo/Tipo";
+import AsignacionVar from "./AsignacionVar";
 import Declaracion from "./Declaracion";
 import Metodo from "./Metodo";
 
@@ -36,11 +37,24 @@ export default class Llamada extends Instruccion {
             for (let i = 0; i < busqueda.parametros.length; i++) {
                 let declaracionParametro = new Declaracion(
                     busqueda.parametros[i].tipo, this.linea, this.col,
-                    busqueda.parametros[i].id, this.parametros[i]
-                )
+                    busqueda.parametros[i].id)
 
+                // declaramos variable en el ambito de la funcion
                 let resultado = declaracionParametro.interpretar(arbol, newTabla)
                 if (resultado instanceof Errores) return resultado
+
+                // interpretamos su valor
+                let valor = this.parametros[i].interpretar(arbol, tabla)
+                if (valor instanceof Errores) return valor
+
+                let variable = newTabla.getVariable(busqueda.parametros[i].id)
+                if (variable == null) return new Errores("Semantico", "algo salio mal", this.linea, this.col)
+                if (variable.getTipo().getTipo() != this.parametros[i].tipoDato.getTipo()) {
+                    return new Errores("Semantico", "algo salio mal", this.linea, this.col)
+                }
+                variable.setValor(valor)
+                console.log(variable)
+                console.log(valor)
             }
             // interpretar la funcion a llamar
             let resultadoFuncion: any = busqueda.interpretar(arbol, newTabla)
@@ -49,3 +63,26 @@ export default class Llamada extends Instruccion {
         }
     }
 }
+
+/*
+    La llamada funciona ligeramente diferente al run
+    El run viene unicamente en el ambito global por lo
+    que se puede interpretar el valor con la newTabla sin ningun inconveniente
+    Ya que solo posee acceso al entorno local y global
+
+    Pero la llamada normal se deben usar 2 entornos
+    1. El entorno donde se llama a la funcion (tabla)
+    2. El entorno que se crea para la función (newTabla)
+
+    Entonces necesitamos que el valor del parametro se interprete con 
+    el entorno "tabla", ya que podriamos pasar una variable que este en
+    este entorno y poder pasar el valor a la funcion a llamar
+    El parametro se declara siempre en la newTabla
+    Y aqui no podemos colocar que la declaración se haga con el valor en newTabla
+    ya que newTabla no tiene contacto con tabla
+    newTabla solo tiene acceso al ambito global y al local, no a tabla
+
+
+    Por eso es que declaro la variable con su valor por default en el ambito newTabla
+    Luego interpreto el valor con tabla y finalmente hago la actualización del valor
+*/
